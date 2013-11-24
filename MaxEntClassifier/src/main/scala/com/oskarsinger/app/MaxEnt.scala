567a49d6bab392df52ea6266a531241d3d683d6f
Oskar Singer <oskarsinger@gmail.com>
2013-11-24 17:14:20 -0500

    Adding complete maven project
    Adding the complete maven project directory with pom.xml and the source
    files in the proper folders in the project directory

--- MaxEntClassifier/src/main/scala/com/oskarsinger/app/MaxEnt.scala
+++ MaxEntClassifier/src/main/scala/com/oskarsinger/app/MaxEnt.scala
@@ -21,12 +21,12 @@ class MaxEnt {
         file <- (new File(c)).listFiles.toList.map(Source.fromFile(_))
         word <- file.mkString.split(" ").map(_.toLowerCase)
       }{
-        constraints(cls -> word) += 1
+        constraints(c -> word) += 1
         classes.foreach( cls => model(cls -> word) = 0 )
       }
     }
 
-    val optimizedLambdas = CGWrapper.fminNCG(value, gradient, model)
+    val optimizedLambdas = CGWrapper.fminNCG(value, gradient, model).toList
 
     model.keys.zip(optimizedLambdas).foldLeft(Map[Tuple2[String, String], Double]())(_+_) 
   }
@@ -40,13 +40,15 @@ class MaxEnt {
   def value(lambdas: Array[Double], keys: List[Tuple2[String, String]], dirs: List[String]): Double = {
     val model = keys.zip(lambdas).foldLeft(Map[Tuple2[String, String], Double]())(_+_)
     val classes = dirs
-    val totalLogProb = 0
+    val totalLogProb = (for{
+                            c <- classes
+                            file <- (new File(c)).listFiles.toList
+                        } yield Math.log(classify(model, classes, file)(c))
+                       ).toList.reduceLeft(_+_)
+    
+    val gaussianPrior = lambdas.toList.map(Math.log(_)).reduceLeft(_+_) //TODO: Figure out how to actually do the Gaussian prior
 
-    -((for{
-        c <- classes
-        file <- (new File(c)).listFiles.toList
-    } yield Math.log(classify(model, classes, file)(c))
-    ).toList.reduceLeft(_+_) + lambdas.toList.map(Math.log(_)).reduceLeft(_+_)) //TODO: Figure out how to actually do the Gaussian prior
+    -(totalLogProb + gaussianPrior)
   }
 
   def classify(model: Map[Tuple2[String, String], Double], classes: List[String], file: File): Map[String, Double] = {
@@ -60,20 +62,24 @@ class MaxEnt {
     val exScores = scores.map( score => score._1 -> Math.exp(score._2 - minimum) )
     val normalizer = exScores.values.reduceLeft(_+_)
 
-    exScores.map( score => score._1 -> score._2/normalizer ) //.toList.sortWith( (x,y) => x._2 > y._2 )
+    val normScores = exScores.map( score => score._1 -> score._2/normalizer ) //.toList.sortWith( (x,y) => x._2 > y._2 )
+
+    assert( normScores.values.reduceLeft(_+_) == 1 )
+
+    normScores
   }
 
   private object CGWrapper {
     def fminNCG(value: (Array[Double], List[Tuple2[String, String]], List[String]) => Double,
                 gradient: (Array[Double], List[Tuple2[String, String]], List[String]) => Array[Double],
                 oldModel: Map[Tuple2[String, String], Double]
-               ): List[Double] = {
+               ): Array[Double] = {
       val initialWeights = oldModel.values
       val keys = oldModel.keys.toList
       val dirs = keys.map( x => x._1 )
       val model = new Parameters { val weights = Weights(new DenseTensor1(initialWeights.size)) }
 
-      model.weights.value := initialWeights
+      model.weights.value := initialWeights.toArray
 
       val optimizer = new ConjugateGradient
       val gradientMap = model.parameters.blankDenseMap
